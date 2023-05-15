aed3b518210c1910861310a983ed6205a9fc7186
Parufito <parufito@gmail.com>
2023-05-15 13:04:56 +0200

    Feature/docker compose (#1)
    * Add docker compose
    restrict filenames
    first rss.xml file
    
    * rss template + test
    
    * add nginx
    
    * add logo
    fix ports
    
    * clean file

--- scripts/playlist.sh
+++ scripts/playlist.sh
@@ -1,4 +1,4 @@
 # Agafa el primer paràmetre de la execució del script des del shell $1 (ho vaig canviar, abans ho pillava de una variable d'entorn $URL, però no hi podia accedir des del bot de telegram node-telegram-bot-api)
 # --ppa fa un crop de la imatge, pq a youtube és 16:9 i els discos són squared.
-yt-dlp --extract-audio --embed-thumbnail -o "music/%(artist)s/%(album)s/%(playlist_index)s - %(title)s.%(ext)s" --audio-format mp3 --add-metadata  $1 --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\""
+yt-dlp --extract-audio --restrict-filenames --embed-thumbnail -o "music/%(artist)s/%(album)s/%(playlist_index)s - %(title)s.%(ext)s" --audio-format mp3 --add-metadata  $1 --ppa "EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\""
 
